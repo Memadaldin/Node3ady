36c61ea2bc2480b2ee8b851819484c3601e4ed4d
Memadaldin <m.emadaldin9@gmail.com>
2018-04-18 21:33:35 +0200

    adding_remotedb

--- db-Initialize.js
+++ db-Initialize.js
@@ -1,2 +1,6 @@
 const mongoose= require('mongoose');
-mongoose.connect('mongodb://localhost:27017/mySample');
+let mongo_url='mongodb://localhost:27017/iti-emad';
+if(process.env.NODE_ENV === 'production'){
+    mongo_url = 'mongodb://MEmadaldin-UI:phoenix!!@ds147589.mlab.com:47589/iti-emad'
+}
+mongoose.connect(mongo_url);
